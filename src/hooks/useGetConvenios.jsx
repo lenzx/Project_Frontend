ddf2130988b806f9d49cc13a4f76c7e34421636b
Leodan Valda <leo_1998@live.cl>
2023-11-11 19:21:14 -0300

    http aws

--- src/hooks/useGetConvenios.jsx
+++ src/hooks/useGetConvenios.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-// process.env.REACT_APP_API_URL
 const useGetConvenios = () => { 
     const [convenios, setConvenios] = useState([]);
 
     useEffect(() => {
         const getConvenios = async () => {
-            const { data } = await axios.get(`https://54.94.65.18:8000/api/v1/convenios/`);
+            const { data } = await axios.get(`http://54.233.113.213/api/v1/convenios/`);
             setConvenios(data);
         }
         getConvenios();
