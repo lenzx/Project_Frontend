528de49a8b1cb10f1b8bd86be854481c767fec36
Leodan Valda <leo_1998@live.cl>
2023-11-13 03:10:05 -0300

    cambio url produccion

--- src/markay/api/endpoint.js
+++ src/markay/api/endpoint.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   window.location.hostname === "localhost" // Comprobar si estás en el entorno local
     ? "http://localhost:8000"
-    : "http://54.233.113.213"; // URL de producción
+    : "http://15.228.23.198"; // URL de producción
