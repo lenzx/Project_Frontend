3ea1d14692385a19bda1ccda7d5f9b116ae3cdbf
kelly-riv <kelly.rivera@inacapmail.cl>
2023-11-17 12:39:21 -0300

    arreglo logo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
                 <figure>
-                    <img src={logo} alt="logo" />
+                    <img src={logo} alt="Markay" className="logo"/>
                 </figure>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
