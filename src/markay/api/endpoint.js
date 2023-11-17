99de361274a1c6779a6e7ac097fc4d16a5a0dc7f
Leodan Horacio Valda Choque <leo_1998@live.cl>
2023-11-17 17:31:07 -0300

    cambio url

--- src/markay/api/endpoint.js
+++ src/markay/api/endpoint.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   window.location.hostname === "localhost" // Comprobar si estás en el entorno local
     ? "http://localhost:8000"
-    : "http://15.228.23.198"; // URL de producción
+    : "http://18.228.46.92"; // URL de producción
