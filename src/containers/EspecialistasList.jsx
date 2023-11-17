c0b07c884513d33030606a7f78f269fd33c11ca6
kelly-riv <kelly.rivera@inacapmail.cl>
2023-11-17 11:35:56 -0300

    especialistas arreglo

--- src/containers/EspecialistasList.jsx
+++ src/containers/EspecialistasList.jsx
@@ -10,6 +10,16 @@ const EspecialistasList = () => {
                 <div className="col-md-12">
                     <Especialistas
                     imagen={especialista}
+                    nombre={"Patricio Andres Donoso Pinto"}
+                    descripcion={"kinesiologo- Representante 3DPRO plantillas ortopédicas- Trainfes- Terapeuta Puncion seca"}
+                    lugar={"- Farfalla Medica"}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <Especialistas
+                    imagen={especialista}
                     nombre={"Ulises Williams Castro Carvajal                    "}
                     descripcion={"Kinesiólogo – Licenciado en kinesiología - Técnica en Punción Seca – Masoterapeuta Deportivo – Trainfes - Especialista en rehabilitación traumatológica y respiratoria"}
                     lugar={"- Clínica del Mirador"}
@@ -20,10 +30,10 @@ const EspecialistasList = () => {
             <div className="row">
                 <div className="col-md-12">
                     <Especialistas
-                    imagen={marcos}
+                    imagen={especialista}
                     nombre={"Jhonny Cristopher Castillo Parra"}
                     descripcion={"Kinesiólogo – Especialista entrenamiento deportivo – Manejo en Columna Vertebral – Rehabilitación respiratoria"}
-                    lugar={"- Farfalla Medica"}
+                    lugar={"- Diagno salud Calama"}
                     />
                 </div>
             </div>
@@ -31,7 +41,7 @@ const EspecialistasList = () => {
             <div className="row">
                 <div className="col-md-12">
                     <Especialistas
-                    imagen={marcos}
+                    imagen={especialista}
                     nombre={"Rafael Alejandro Silva Valenzuela"}
                     descripcion={"Doctor en Quiropraxia – Master en técnicas osteopáticas del aparato locomotor – Osteópata de Academía Latinoamericana de Osteopatía"}
                     lugar={"- Farfalla Medica"}
@@ -42,13 +52,15 @@ const EspecialistasList = () => {
             <div className="row">
                 <div className="col-md-12">
                     <Especialistas
-                    imagen={marcos}
+                    imagen={especialista}
                     nombre={"Jeremy Andres Zambrano Encalada"}
                     descripcion={"kinesiologo- diplomado en neurorrehabilitación- especialista en rehabilitación traumatologica y respiratoria- entrenamiento deportivo"}
-                    lugar={"- Farfalla Medica"}
+                    lugar={"- Torre cobre medical"}
                     />
                 </div>
             </div>
+            
+            
 
         </>
     );
